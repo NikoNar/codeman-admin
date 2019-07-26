5fde405a0b2c0a3b1caeaa3998e974c4388ffaef
Yuri Avanesyan <y.avanesyan@gmail.com>
2019-07-26 17:49:46 +0400

    fixes

--- src/views/contact_us.blade.php
+++ src/views/contact_us.blade.php
@@ -311,7 +311,7 @@
                                 <tr>
                                     <td>
                                         <p>{{$content['name']}}</p>
-                                        <p>{{$content['content']}}</p>
+                                        <p>{{$content['message']}}</p>
                                         <!-- <p>Sometimes you just want to send a simple HTML email with a simple design and clear call to action. This is it.</p>
                                         <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
                                           <tbody>
